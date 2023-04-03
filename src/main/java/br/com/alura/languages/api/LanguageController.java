a968a5a23c59a68d22e41d7f3bf8522a80471229
Luisa <luisa.santos042@gmail.com>
2023-04-03 10:03:34 -0300

    add language ranking

--- src/main/java/br/com/alura/languages/api/LanguageController.java
+++ src/main/java/br/com/alura/languages/api/LanguageController.java
@@ -1,13 +1,28 @@
 package br.com.alura.languages.api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
-    
-    @GetMapping(value="/hello")
-    public String getLanguage() {
+
+    public List<Language> languages = List.of(
+            new Language("Java",
+                    "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
+                    1),
+            new Language("PHP",
+                    "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
+                    2));
+
+    @GetMapping("/hello")
+    public String getHello() {
         return "hello";
+    }
+
+    @GetMapping("/ranking")
+    public List<Language> getLanguage() {
+        return languages;
     }
 }
